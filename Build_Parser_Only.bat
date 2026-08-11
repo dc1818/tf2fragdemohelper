3bb3fb680a8292071383729e7ebf349c2d5ac9ef
dc1818 <45833384+dc1818@users.noreply.github.com>
2026-08-11 12:27:35 -0300

    Allow quiet parser sub-builds

--- Build_Parser_Only.bat
+++ Build_Parser_Only.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal EnableExtensions
 title Build TF2 STV Parser
+set "QUIET="
+if /I "%~1"=="--quiet" set "QUIET=1"
 set "PARSER_ROOT=%~dp0parser"
 if not exist "%PARSER_ROOT%\Cargo.toml" goto :missing
 where cargo >nul 2>&1
@@ -9,6 +11,7 @@ pushd "%PARSER_ROOT%"
 cargo build --release --locked --bin export_all
 if errorlevel 1 (popd & goto :fail)
 popd
+if defined QUIET exit /b 0
 echo.
 echo PARSER BUILD SUCCEEDED.
 echo You can now open TF2_STV_Parser_GUI.exe.
@@ -22,5 +25,6 @@ echo ERROR: cargo was not found. Install Rust from https://rustup.rs/ and reopen
 :fail
 echo.
 echo BUILD FAILED. The first error above is the useful one.
+if defined QUIET exit /b 1
 pause
 exit /b 1
